bd4f4c9cd9e158088afd9785ea4f16af439c0ffc
Cameron Bilyeu <158659699+magicat8@users.noreply.github.com>
2025-07-26 15:00:10 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const suits = ['♠', '♥', '♦', '♣'];
+const values = [
+  'A', '2', '3', '4', '5', '6', '7',
+  '8', '9', '10', 'J', 'Q', 'K'
+];
+
+// Generate full 52-card deck
+let deck = [];
+for (let suit of suits) {
+  for (let value of values) {
+    deck.push({ suit, value });
+  }
+}
+
+// Remove 1 random card to make 51
+deck.splice(Math.floor(Math.random() * deck.length), 1);
+
+// Shuffle using Fisher-Yates
+for (let i = deck.length - 1; i > 0; i--) {
+  let j = Math.floor(Math.random() * (i + 1));
+  [deck[i], deck[j]] = [deck[j], deck[i]];
+}
+
+// Deal 25 cards
+const hand = deck.slice(0, 25);
+
+const container = document.getElementById('card-container');
+
+hand.forEach(card => {
+  const div = document.createElement('div');
+  div.className = 'card';
+  if (card.suit === '♥' || card.suit === '♦') {
+    div.classList.add('red');
+  }
+  div.innerHTML = `${card.value}<br>${card.suit}`;
+  container.appendChild(div);
+});
